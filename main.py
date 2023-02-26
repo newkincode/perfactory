6b898dc1a12efa9cd2ace52ebc114b2cb45c2c8e
Gongback <94734506+newkincode@users.noreply.github.com>
2023-02-26 21:42:15 +0900

    yes... its good

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ SKYBLUE = (113, 199, 245)
 # 타일 이미지
 tileImg = pygame.image.load("asset/img/tile.png")
 conveyorBeltImg = pygame.image.load("asset/img/conveyor_belt.png")
+# conveyorBeltImg1.set_alpha(128)
 
 # 아이템
 itemList = [2]
@@ -30,33 +31,35 @@ conveyorBeltPosList = []
 pygame.display.set_caption(f"SFG2 {var}! - by newkini") # 창 이름
 #pygame.mouse.set_cursor((32, 32),(0, 0),cusImg,(0,0))#(32,32),(0,0),(0,0),
 print(pygame.cursors.arrow)
-
 while runing:
     musPos = pygame.mouse.get_pos()
+    musTile = [math.trunc(musPos[0]/32), math.trunc(musPos[1]/32)]
+    musTilePos = [32*musTile[0], 32*musTile[1]]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runing = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_presses = pygame.mouse.get_pressed()
             if mouse_presses[0]:
-                musTile = [math.trunc(musPos[0]/32), math.trunc(musPos[1]/32)]
-                musTilePos = [musTile[0]*32, musTile[1]*32]
-                conveyorBeltPosList.append(musTilePos)
                 tilemap[musTile[1]][musTile[0]] = 2
-                print(musTilePos)
-
+                print(musTile)
+    print(musTilePos)
     screen.fill(SKYBLUE)  # 화면 채우기
     tilePos = [0, 0]
     for line in tilemap:
         for tile in line:
             if (tile == 1) or (tile in itemList):
                 screen.blit(tileImg, tilePos)
+            if tile == 2:
+                screen.blit(conveyorBeltImg, tilePos)
             tilePos[0] += 32
         tilePos[1] += 32
         tilePos[0] = 0
-    pygame.display.update()  # 화면 업데이트
+    
+    conveyorBeltImg.set_alpha(128)
+    screen.blit(conveyorBeltImg,musTilePos)
+    conveyorBeltImg.set_alpha(255)
 
-    for i in conveyorBeltPosList:
-        screen.blit(conveyorBeltImg, i)
+    pygame.display.update()  # 화면 업데이트
 
 pygame.quit()
