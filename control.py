09bf4359d6956f130ec004c6c99c9b8d635d2f8a
newkincode <maainnewkin59@gmail.com>
2023-03-01 11:10:31 +0900

    아이템 추가

---
+++ control.py
@@ -0,0 +1,38 @@
+import tkinter
+import json
+
+window=tkinter.Tk()
+
+conveyorBeltImg = tkinter.PhotoImage(file="asset/img/conveyor_belt.png")
+conveyorBeltImgUL = tkinter.PhotoImage(file="asset/img/conveyor_belt_ul.png")
+conveyorBeltImgLR = tkinter.PhotoImage(file="asset/img/conveyor_belt_lr.png")
+selectItem = 0
+
+def setSelect(a):
+    save = open("data/playerData.json","w")
+    save.write(json.dumps({"selectItem":a}))
+    save.close()
+
+window.title("YUN DAE HEE")
+window.geometry("640x400")
+window.resizable(False, False)
+
+canvas = tkinter.Canvas(window,width=640, height=400)
+canvas.place(x=50, y=30)
+
+canvas.create_rectangle(10, 10, 278, 278, outline='black', width=3)
+
+selectText=tkinter.Label(window, text="아이템 선택")
+selectText.place(x=50, y=30)
+
+conveyorBeltBtn = tkinter.Button(window,image=conveyorBeltImg, command=lambda: setSelect(0))
+conveyorBeltBtn.place(x=70, y=62)
+
+conveyorBeltULBtn = tkinter.Button(window,image=conveyorBeltImgUL, command=lambda: setSelect(1))
+conveyorBeltULBtn.place(x=122, y=62)
+
+conveyorBeltLRBtn = tkinter.Button(window,image=conveyorBeltImgLR, command=lambda: setSelect(2))
+conveyorBeltLRBtn.place(x=174, y=62)
+
+
+window.mainloop()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import lib.tile as tiles
 import math
+import json
 
 # mouses = mus
 
@@ -20,32 +21,38 @@ SKYBLUE = (113, 199, 245)
 # 타일 이미지
 tileImg = pygame.image.load("asset/img/tile.png")
 conveyorBeltImg = pygame.image.load("asset/img/conveyor_belt.png")
+conveyorBeltImgUL = pygame.image.load("asset/img/conveyor_belt_ul.png")
+conveyorBeltImgLR = pygame.image.load("asset/img/conveyor_belt_lr.png")
 # conveyorBeltImg1.set_alpha(128)
 
 # 아이템
-itemList = [2]
-selectItem = 0
+itemList = [2,3,4]
+selectItem = 1
 conveyorBeltPosList = []
+
+def opneDef():
+    global selectItem
+    try:
+        save = open("data/playerData.json","r")
+        saveData = json.load(save)
+        selectItem = saveData["selectItem"]
+    except:pass
 
 # 세팅
 pygame.display.set_caption(f"SFG2 {var}! - by newkini") # 창 이름
 #pygame.mouse.set_cursor((32, 32),(0, 0),cusImg,(0,0))#(32,32),(0,0),(0,0),
 print(pygame.cursors.arrow)
 while runing:
+    opneDef()
     musPos = pygame.mouse.get_pos()
     musTile = [math.trunc(musPos[0]/32), math.trunc(musPos[1]/32)]
     musTilePos = [32*musTile[0], 32*musTile[1]]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runing = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_presses = pygame.mouse.get_pressed()
-            if mouse_presses[0]:
-                tilemap[musTile[1]][musTile[0]] = 2
-                print(musTile)
     try:
         if pygame.mouse.get_pressed()[0] == 1:
-            tilemap[musTile[1]][musTile[0]] = 2
+            tilemap[musTile[1]][musTile[0]] = itemList[selectItem]
             print(musTile)
     except:pass
     print(musTilePos)
@@ -57,6 +64,10 @@ while runing:
                 screen.blit(tileImg, tilePos)
             if tile == 2:
                 screen.blit(conveyorBeltImg, tilePos)
+            if tile == 3:
+                screen.blit(conveyorBeltImgUL, tilePos)
+            if tile == 4:
+                screen.blit(conveyorBeltImgLR, tilePos)
             tilePos[0] += 32
         tilePos[1] += 32
         tilePos[0] = 0
