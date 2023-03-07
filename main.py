13c56d47f8789709e97df4ac621afecef759a9fc
newkincode <maainnewkin59@gmail.com>
2023-03-07 22:44:26 +0900

    add type

--- main.py
+++ main.py
@@ -8,34 +8,34 @@ import json
 pygame.init()
 
 # 변수
-hw = (960, 640)
+hw: tuple = (960, 640) # 창크기변수
 screen = pygame.display.set_mode(hw)
 runing = True
 var = "alpha 0.2"
 tilemap = tiles.tileMap
 # 마우스
-musPos = pygame.mouse.get_pos()
-cusImg = pygame.image.load("asset/img/mouseCus.png")
+musPos: tuple = pygame.mouse.get_pos()
+cusImg: pygame.Surface = pygame.image.load("asset/img/mouseCus.png")
 # 색
 SKYBLUE = (113, 199, 245)
 # 타일 이미지
-tileImg = pygame.image.load("asset/img/tile.png")
-conveyorBeltImg = pygame.image.load("asset/img/conveyor_belt.png")
-conveyorBeltImgUL = pygame.image.load("asset/img/conveyor_belt_ul.png")
-conveyorBeltImgLR = pygame.image.load("asset/img/conveyor_belt_lr.png")
-# conveyorBeltImg1.set_alpha(128)
+tileImg: pygame.Surface = pygame.image.load("asset/img/tile.png")
+conveyorBeltImgs: dict[str, pygame.Surface] = {
+    "downUp" : pygame.image.load("asset/img/conveyor_belt.png"),
+    "upLeft" : pygame.image.load("asset/img/conveyor_belt_up_left.png"),
+    "leftRight" : pygame.image.load("asset/img/conveyor_belt_left_right.png")
+}
 
 # 아이템
-itemList = [2,3,4]
-selectItem = 1
-conveyorBeltPosList = []
+itemList: list = [2,3,4]
+selectItem: int = 1
 
 def opneDef():
     global selectItem
     try:
         save = open("data/playerData.json","r")
-        saveData = json.load(save)
-        selectItem = saveData["selectItem"]
+        saveData: dict = json.load(save)
+        selectItem: int = saveData["selectItem"]
     except:pass
 
 # 세팅
@@ -43,10 +43,14 @@ pygame.display.set_caption(f"SFG2 {var}! - by newkini") # 창 이름
 pygame.mouse.set_visible(False)
 
 while runing:
+    # 함수
     opneDef()
+
+    # 변수
     musPos = pygame.mouse.get_pos()
-    musTile = [math.trunc(musPos[0]/32), math.trunc(musPos[1]/32)]
-    musTilePos = [32*musTile[0], 32*musTile[1]]
+    musTile: list = [math.trunc(musPos[0]/32), math.trunc(musPos[1]/32)]
+    musTilePos: list = [32*musTile[0], 32*musTile[1]]
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runing = False
@@ -62,18 +66,18 @@ while runing:
             if (tile == 1) or (tile in itemList):
                 screen.blit(tileImg, tilePos)
             if tile == 2:
-                screen.blit(conveyorBeltImg, tilePos)
+                screen.blit(conveyorBeltImgs["downUp"], tilePos)
             if tile == 3:
-                screen.blit(conveyorBeltImgUL, tilePos)
+                screen.blit(conveyorBeltImgs["upLeft"], tilePos)
             if tile == 4:
-                screen.blit(conveyorBeltImgLR, tilePos)
+                screen.blit(conveyorBeltImgs["leftRight"], tilePos)
             tilePos[0] += 32
         tilePos[1] += 32
         tilePos[0] = 0
     
-    conveyorBeltImg.set_alpha(128)
-    screen.blit(conveyorBeltImg,musTilePos)
-    conveyorBeltImg.set_alpha(255)
+    conveyorBeltImgs["downUp"].set_alpha(128)
+    screen.blit(conveyorBeltImgs["downUp"],musTilePos)
+    conveyorBeltImgs["downUp"].set_alpha(255)
     screen.blit(cusImg,musPos)
 
     pygame.display.update()  # 화면 업데이트
