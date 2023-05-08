0736512bebdf9ff0f16911f574444054a9d17361
newkincode <maainnewkin59@gmail.com>
2023-05-08 18:19:17 +0900

    타일맵 기능 완성

--- lib/factory.py
+++ lib/factory.py
@@ -3,9 +3,11 @@ from enum import Enum, auto
 class Tiles(Enum):
     EMPTY = auto()
 
-    class Conveyor(Enum):
-        UP = auto()
-        UP_LEFT = auto()
-        UP_RIGHT = auto()
-        DOWN_LEFT = auto()
-        DOWN_RIGHT = auto()
+class Conveyor(Enum):
+    UP = "img/conveyor/up.png"
+    UP_LEFT = "img/conveyor/up_left.png"
+    UP_RIGHT = "img/conveyor/up_right.png"
+    DOWN_LEFT = "img/conveyor/down_left.png"
+    DOWN_RIGHT = "img/conveyor/down_right.png"
+
+tilemap = [[Tiles.EMPTY for i in range(0, 30)] for i in range(0, 17)]
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import pygame
+from lib import factory
 pygame.init()
 
 # init
-screen: pygame.Surface = pygame.display.set_mode((800, 600))
-var: tuple[str, int, int, int] = ("alpha", 0, 0, 0)
+screen: pygame.Surface = pygame.display.set_mode((32*30, 32*17))
+var: tuple[str, int, int, int] = ("alpha", 0, 0, 1)
 pygame.display.set_caption(f"sfg2 {var[0]} {var[1]}.{var[2]}.{var[3]} by newkini")
 clock = pygame.time.Clock()
 is_running = True
 
 # main loop
 while is_running:
+    clock.tick(60)
+
     # events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
 
+    # draw
+    screen.fill(pygame.Color(255,255,255))
+
+    # tile
+    tilepos = pygame.Vector2(0, 0)
+    for line in factory.tilemap:
+        for tile in line:
+            if tile == factory.Tiles.EMPTY:
+                pass
+            else:
+                screen.blit(pygame.image.load(tile.value), (tilepos.x*32,tilepos.y*32))
+            tilepos.x += 1
+        tilepos.y += 1
+        tilepos.x = 0
+
     # update
     pygame.display.update()
-    clock.tick(60)
+    
 pygame.quit()
