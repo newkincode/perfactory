03f13c20131d8cbfb673c5ed2a4d0c65dccba62a
newkincode <maainnewkin59@gmail.com>
2023-05-08 18:37:12 +0900

    타일드로우

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from lib import factory
 pygame.init()
 
@@ -9,14 +10,28 @@ pygame.display.set_caption(f"sfg2 {var[0]} {var[1]}.{var[2]}.{var[3]} by newkini
 clock = pygame.time.Clock()
 is_running = True
 
+# pygame.math.Vector2(
+#             math.trunc(self.pos.x/32),
+#             math.trunc(self.pos.y/32)
+#         )
+
 # main loop
 while is_running:
     clock.tick(60)
+
+    # update var
+    mouse_pos = pygame.mouse.get_pos()
+    select_tile = factory.Conveyor.UP
 
     # events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
+    
+    # mouse events
+    if pygame.mouse.get_pressed()[0] == 1:
+        tPos = pygame.Vector2(math.trunc(mouse_pos[0]/32), math.trunc(mouse_pos[1]/32))
+        factory.tilemap[int(tPos.y)][int(tPos.x)] = select_tile
 
     # draw
     screen.fill(pygame.Color(255,255,255))
